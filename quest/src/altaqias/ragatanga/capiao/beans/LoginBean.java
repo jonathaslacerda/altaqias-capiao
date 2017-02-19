17e3e7e19a0ec54fde171c85ca227100ff109864
virtus sistemas <virtus sistemas@VirtusSistemas>
2017-02-19 09:11:39 -0300

    a

--- quest/src/altaqias/ragatanga/capiao/beans/LoginBean.java
+++ quest/src/altaqias/ragatanga/capiao/beans/LoginBean.java
@@ -1,5 +1,7 @@
 package altaqias.ragatanga.capiao.beans;
 
+import java.security.NoSuchAlgorithmException;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
@@ -7,6 +9,7 @@ import javax.faces.bean.RequestScoped;
 import com.google.gson.JsonSyntaxException;
 
 import altaqias.ragatanga.apiclient.control.ClienteController;
+import altaqias.ragatanga.capiao.utils.CryptUtils;
 import altaqias.ragatanga.capiao.utils.FacesUtils;
 import altaqias.ragatanga.to.ClienteAutenticarResponse;
 import altaqias.ragatanga.to.MensagemTO;
@@ -21,26 +24,32 @@ public class LoginBean {
 	private String email;
 	@Getter @Setter
 	private String senha;
-	
+
 	@ManagedProperty(value="#{SessaoBean}")
 	@Getter @Setter
 	private SessaoBean sessao;
 
 	public String logar(){
 		try{
-			ClienteAutenticarResponse response = ClienteController.autenticar(this.email, this.senha);
-			if(response.getMensagem().getCodigo().equals(MensagemTO.CODIGO_SUCESSO)){
-				this.sessao.setCliente(response.getCliente());
-				return "home?faces-redirect=true";
+			String senhaMD5 = CryptUtils.toMD5(this.senha);
+			ClienteAutenticarResponse response = ClienteController.autenticar(this.email, senhaMD5);
+			if(response != null){
+				if(response.getMensagem().getCodigo().equals(MensagemTO.CODIGO_SUCESSO)){
+					this.sessao.setCliente(response.getCliente());
+					return "home?faces-redirect=true";
+				}else{
+					FacesUtils.alertGrow(response.getMensagem().getMensagem());
+				}
+			}else{
+				FacesUtils.alertGrow("Opss... Ocorreu um erro. Tenta novamente daqui a pouco.");
 			}
-			FacesUtils.alertGrow(response.getMensagem().getMensagem());
-		}catch(JsonSyntaxException e){
+		}catch(JsonSyntaxException | NoSuchAlgorithmException e){
 			e.printStackTrace();
 			FacesUtils.alertGrow(e.getMessage());
 		}
 		return "";
 	}
-	
+
 	public String sair(){
 		this.sessao.logout();
 		return "../login?faces-redirect=true";
--- quest/src/altaqias/ragatanga/capiao/beans/ViagensBean.java
+++ quest/src/altaqias/ragatanga/capiao/beans/ViagensBean.java
@@ -1,14 +1,19 @@
 package altaqias.ragatanga.capiao.beans;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 
 import altaqias.ragatanga.apiclient.control.QuestController;
+import altaqias.ragatanga.capiao.utils.FacesUtils;
+import altaqias.ragatanga.model.Inscricao;
 import altaqias.ragatanga.model.Quest;
+import altaqias.ragatanga.to.MensagemTO;
+import altaqias.ragatanga.to.QuestEntrarResponse;
+import altaqias.ragatanga.to.QuestUltimasResponse;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,20 +22,66 @@ import lombok.Setter;
 public class ViagensBean {
 
 	@Getter @Setter
-	List<Quest> quests;
+	private List<Quest> viagens;
+
+	@Getter @Setter
+	private String erro;
 	
+	@ManagedProperty(value="#{SessaoBean}")
+	@Getter @Setter
+	private SessaoBean sessao;
+
 	@PostConstruct
 	public void init() {
-//		this.quests = this.carregarUltimasQuests();
-		this.quests = quests = new ArrayList<Quest>();
+		try{
+			QuestUltimasResponse response = QuestController.ultimas();
+			if(response != null){
+				if(response.getMensagem().getCodigo().equals(MensagemTO.CODIGO_SUCESSO)){
+					this.viagens = response.getQuests();
+					if(this.viagens == null || this.viagens.isEmpty()){
+						this.erro = "Nao existem viagens disponiveis.";
+					}
+				}else{
+					this.erro = response.getMensagem().getMensagem();
+				}
+			}else{
+				this.erro = "Erro no servidor. Nao foi possivel listar as viagens disponiveis.";
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+			this.erro = "Erro no servidor. Nao foi possivel listar as viagens disponiveis.";
+		}
+	}
+	
+	public void irTambem(Integer idQuest){
+		try{
+			QuestEntrarResponse response = QuestController.entrar(this.sessao.getCliente().getId(), idQuest);
+			if(response != null){
+				if(response.getMensagem().getCodigo().equals(MensagemTO.CODIGO_SUCESSO)){
+					FacesUtils.sucessGrow("Uhuu!!! Sua inscricao foi solicitada com sucesso, existe uma grande possibilidade de voce ir junto nessa. Aguarde a aprovacao do admin desda viagem.");
+					QuestUltimasResponse quResponse = QuestController.ultimas();
+					this.viagens = quResponse.getQuests();
+				}else{
+					FacesUtils.alertGrow(response.getMensagem().getMensagem());
+				}
+			}else{
+				FacesUtils.alertGrow("Erro no servidor. Nao foi possivel se inscrever nessa viagem.");
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+			FacesUtils.alertGrow("Erro no servidor. Nao foi possivel listar as viagens disponiveis.");
+		}
+	}
+	
+	public boolean inscricaoJaRealizada(){
+		for(Quest q: this.viagens){
+			for(Inscricao i: q.getInscricoes()){
+				if(i.getCliente().getId() == this.sessao.getCliente().getId()){
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
-	@SuppressWarnings("unchecked")
-	private List<Quest> carregarUltimasQuests() {
-		List<Quest> quests = (List<Quest>) QuestController.ultimas();
-		if(quests == null){
-			quests = new ArrayList<Quest>();
-		}
-		return quests;
-	}
 }
