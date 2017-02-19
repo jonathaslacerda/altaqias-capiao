714399a6a316808e9d27d184c5e813f34e869f7f
jonny <jonny@BARBATOS>
2017-02-19 08:51:58 -0300

    erer

--- quest/src/altaqias/ragatanga/capiao/beans/LoginBean.java
+++ quest/src/altaqias/ragatanga/capiao/beans/LoginBean.java
@@ -1,5 +1,7 @@
 package altaqias.ragatanga.capiao.beans;
 
+import java.security.NoSuchAlgorithmException;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
@@ -7,6 +9,7 @@ import javax.faces.bean.RequestScoped;
 import com.google.gson.JsonSyntaxException;
 
 import altaqias.ragatanga.apiclient.control.ClienteController;
+import altaqias.ragatanga.capiao.utils.CryptUtils;
 import altaqias.ragatanga.capiao.utils.FacesUtils;
 import altaqias.ragatanga.to.ClienteAutenticarResponse;
 import altaqias.ragatanga.to.MensagemTO;
@@ -28,13 +31,14 @@ public class LoginBean {
 
 	public String logar(){
 		try{
-			ClienteAutenticarResponse response = ClienteController.autenticar(this.email, this.senha);
+			String senhaMD5 = CryptUtils.toMD5(this.senha);
+			ClienteAutenticarResponse response = ClienteController.autenticar(this.email, senhaMD5);
 			if(response.getMensagem().getCodigo().equals(MensagemTO.CODIGO_SUCESSO)){
 				this.sessao.setCliente(response.getCliente());
 				return "home?faces-redirect=true";
 			}
 			FacesUtils.alertGrow(response.getMensagem().getMensagem());
-		}catch(JsonSyntaxException e){
+		}catch(JsonSyntaxException | NoSuchAlgorithmException e){
 			e.printStackTrace();
 			FacesUtils.alertGrow(e.getMessage());
 		}
